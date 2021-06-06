305876a8b498810f4d4fe701c3a372d3d8ee6059
ghifari-ti <muhammad.ghifari@students.paramadina.ac.id>
2021-06-06 22:27:14 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.sbadmin')
+
+@section('content')
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Welcome!</h1>
+    <div class="row">
+        <div class="col-sm-6 col-12">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Premiere - Tayang hari ini</h6>
+                </div>
+                <div class="mx-2 my-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h4 class="font-weight-bold text-dark">Jurrasic Park</h4>
+                                </div>
+                                <div class="col text-right">
+                                    <h5>Theater 1</h5>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="border text-center rounded mx-1" style="width: 50px;">11:50</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">13:30</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">15:00</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">18:30</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mx-2 my-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h4 class="font-weight-bold text-dark">Jurrasic Park</h4>
+                                </div>
+                                <div class="col text-right">
+                                    <h5>Theater 1</h5>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="border text-center rounded mx-1" style="width: 50px;">11:50</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">13:30</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">15:00</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">18:30</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-12">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3" style="background-color: purple">
+                    <h6 class="m-0 font-weight-bold text-white">Super Premiere - Tayang hari ini</h6>
+                </div>
+                <div class="mx-2 my-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h4 class="font-weight-bold text-dark">Jurrasic Park</h4>
+                                </div>
+                                <div class="col text-right">
+                                    <h5>Theater 1</h5>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="border text-center rounded mx-1" style="width: 50px;">11:50</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">13:30</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">15:00</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">18:30</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mx-2 my-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h4 class="font-weight-bold text-dark">Jurrasic Park</h4>
+                                </div>
+                                <div class="col text-right">
+                                    <h5>Theater 1</h5>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="border text-center rounded mx-1" style="width: 50px;">11:50</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">13:30</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">15:00</div>
+                                <div class="border text-center rounded mx-1" style="width: 50px;">18:30</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,9 @@
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Home</span></a>
+</li>
